b0dc86ee0b60ac0847147df5acc2e3912587b1a4
harold <yafuly@gmail.com>
2023-07-10 13:58:37 +0800

    Update train.sh

--- shell/train.sh
+++ shell/train.sh
@@ -10,11 +10,11 @@ time=$(date +'%m:%d:%H:%M')
 gpu=0
 
 CUDA_VISIBLE_DEVICES=$gpu python3 -u $PROJECT_PATH/fairseq_cli/train.py $DATA_PATH --user-dir $fs_plugins \
-   --keep-interval-updates 10 --save-interval-updates 300 --batch-size-valid 256 --validate-interval-updates 300 --maximize-best-checkpoint-metric \
-   --eval-bleu-remove-bpe --best-checkpoint-metric bleu --log-format simple --log-interval 100 \
-   --eval-bleu --eval-bleu-detok space --keep-last-epochs 1 --keep-best-checkpoints 5  --fixed-validation-seed 7 --ddp-backend=no_c10d \
-   --eval-tokenized-bleu \
-   --task translation \
+	--keep-interval-updates 10 --save-interval-updates 300 --batch-size-valid 256 --validate-interval-updates 300 --maximize-best-checkpoint-metric \
+	--eval-bleu-remove-bpe --best-checkpoint-metric bleu --log-format simple --log-interval 100 \
+	--eval-bleu --eval-bleu-detok space --keep-last-epochs 1 --keep-best-checkpoints 5  --fixed-validation-seed 7 --ddp-backend=no_c10d \
+	--eval-tokenized-bleu \
+	--task translation \
 	--arch at_tree_attn_nonshare --share-all-embeddings \
 	--optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
 	--lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 \
@@ -28,8 +28,8 @@ CUDA_VISIBLE_DEVICES=$gpu python3 -u $PROJECT_PATH/fairseq_cli/train.py $DATA_PA
 	--dropout 0.3 \
 	--fp16 \
 	--tensorboard-logdir $MODEL_PATH/tb \
-   --patience 40 \
-   --dataset-impl 'raw' \
-   --reset-dataloader \
-   --save-dir $MODEL_PATH 2>&1 | tee $MODEL_PATH/train.log.$time
-# remove this line (data-impl） if data is binarized
+	--patience 40 \
+	--dataset-impl 'raw' \
+	--save-dir $MODEL_PATH 2>&1 | tee $MODEL_PATH/train.log.$time
+ 
+# remove ''--dataset-impl 'raw' \'' if your data is binarized
